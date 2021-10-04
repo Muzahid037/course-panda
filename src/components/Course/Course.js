5255f1ca600a6972781d353a489e301cd36ab927
Md. Muzahidul Islam <48602232+Muzahid037@users.noreply.github.com>
2021-10-04 23:37:12 +0600

    course price added

--- src/components/Course/Course.js
+++ src/components/Course/Course.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 import './Course.css';
 
 const Course = (props) => {
-    const {id, title, instructor,img,description } = props.course;
+    const {id, title, instructor,img,description,charges } = props.course;
     function getFirstNWordsFromString(str, n) {
         return str.split(/\s+/).slice(0, n).join(" ");
     }
@@ -13,8 +13,9 @@ const Course = (props) => {
                     <div className="card h-100">
                         <img src={img} className="card-img-top" alt="..."/>
                         <div className="card-body">
-                        <h5 className="card-title">{title}</h5>
+                        <h4 className="card-title">{title}</h4>
                         <p className="card-text">{getFirstNWordsFromString(description,10)}...</p>
+                        <h5>Course Price: {charges}$</h5>
                         <Link className="text-decoration-none" to='/courses'>
                             <button className="btn btn-success text-white">See Details</button>
                         </Link>
